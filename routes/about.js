3f9641198e00278090dddd9fc6d6a73f73bb5b41
TomaszBoron <tomasz.w.boron@gmail.com>
2025-04-23 06:54:53 +0000

    use turbo-strem

--- routes/about.js
+++ routes/about.js
@@ -3,7 +3,22 @@ var router = express.Router();
 
 /* GET about page. */
 router.get('/', function(req, res, next) {
-  res.render('about', { title: 'Trylogia' });
+  res.render('about', { title: 'Kontakt' });
+});
+
+router.post('/', function(req, res, next) {
+  const { message } = req.body
+  res.setHeader("Content-Type", "text/html")
+  res.type('text/vnd.turbo-stream.html');
+  res.send(`
+    <turbo-stream action="append" target="messages">
+      <template>
+        <div id="message_1">
+          ${message}
+        </div>
+      </template>
+    </turbo-stream>
+  `);
 });
 
 module.exports = router;
